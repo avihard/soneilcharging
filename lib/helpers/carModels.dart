1a4cfa54a1623945a01dc8955a5e7378c5a96738
JeetVdevve <jeet.vyas@soneil.com>
2022-09-20 15:06:27 -0400

    Commits
    	- Adding feature to add vehicles

---
+++ lib/helpers/carModels.dart
@@ -0,0 +1,16 @@
+List<String> brands = ['Audi', 'Nissan', 'Rivian', 'Tesla', 'Volvo'];
+
+dynamic models = [
+  {"Maker": "Audi", "Model": "E-tron", "Year": 2016},
+  {"Maker": "Audi", "Model": "E-tron", "Year": 2017},
+  {"Maker": "Audi", "Model": "A3 E-tron", "Year": 2018},
+  {"Maker": "Audi", "Model": "A3 E-tron", "Year": 2020},
+  {"Maker": "Audi", "Model": "A3 E-tron", "Year": 2021},
+  {"Maker": "Audi", "Model": "A3 E-tron", "Year": 2016},
+  {"Maker": "Tesla", "Model": "Model S", "Year": 2016},
+  {"Maker": "Tesla", "Model": "Model S", "Year": 2017},
+  {"Maker": "Tesla", "Model": "Model S", "Year": 2018},
+  {"Maker": "Tesla", "Model": "Model 3", "Year": 2020},
+  {"Maker": "Tesla", "Model": "Model 3", "Year": 2021},
+  {"Maker": "Tesla", "Model": "Model X", "Year": 2016},
+];
